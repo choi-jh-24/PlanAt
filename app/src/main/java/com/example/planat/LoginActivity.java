d172f826c88d42a834dab7855c0cb257eb85f9e8
leeseooo <leeseoooo@naver.com>
2022-03-12 18:37:21 +0900

    feat:로그인 시 시작페이지 일정등록 페이지로 변경

--- app/src/main/java/com/example/planat/LoginActivity.java
+++ app/src/main/java/com/example/planat/LoginActivity.java
@@ -170,7 +170,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                                         }
                                     });
                             finish();
-                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                            Intent intent = new Intent(LoginActivity.this,ScheduleActivity.class);
+                            intent.putExtra("userEmail",email);
+                            startActivity(intent);
+//                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         } else {
                             Toast.makeText(getApplicationContext(), "로그인 실패!", Toast.LENGTH_LONG).show();
                             textviewMessage.setText("로그인 실패 유형\n - password가 맞지 않습니다.\n -서버에러");
