94fd8ff9b21008a5d3351040d9e319b3ce3ccc64
choi-jh-24 <1913507@sookmyung.ac.kr>
2022-03-18 22:27:28 +0900

    서희님 디자인 수정

--- app/src/main/java/com/example/planat/MyPageActivity.java
+++ app/src/main/java/com/example/planat/MyPageActivity.java
@@ -3,20 +3,38 @@ package com.example.planat;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyPageActivity extends AppCompatActivity {
+    ImageView iv;
+    Uri userPhotoUri;
+    String UID;
+    UserModel userModel;
+    final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Map<String, Object> user = new HashMap<>();
 
 
 
@@ -25,11 +43,27 @@ public class MyPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
+        UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        iv = findViewById(R.id.imageView2);
+
+        getUserInfoFromServer();
+
+
+
+
         //프로필버튼
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MypagecheckActivity.class));
+            }
+        });
+
+        findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
@@ -88,5 +122,36 @@ public class MyPageActivity extends AppCompatActivity {
                 selectDialog.show();
             }
         });
+    }
+
+    void getUserInfoFromServer() {
+        //기존 프로필사진 불러와서 이미지뷰에 넣는 코드
+        try{
+            DocumentReference docRef = FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Log.d("FireSever", "onEvent");
+                    userModel = documentSnapshot.toObject(UserModel.class);
+                    if (userModel.getUserPhoto() != null && !userModel.getUserPhoto().equals("null") && !"".equals(userModel.getUserPhoto())) {
+                        FirebaseStorage.getInstance().getReference("userPhoto/" + userModel.getUserPhoto()).getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Uri> task) {
+                                Glide.with(MyPageActivity.this)
+                                        .load(task.getResult())
+                                        .circleCrop()
+                                        .into(iv);
+                            }
+                        });
+
+                    }else{
+                        Toast.makeText(getApplicationContext(), "프로필사진이 없습니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
