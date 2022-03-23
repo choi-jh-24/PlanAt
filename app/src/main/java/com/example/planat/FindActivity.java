342a8bb55b015a225876b98bca10521b17909abb
leeseooo <leeseoooo@naver.com>
2022-03-23 18:37:36 +0900

    fix:비밀번호 찾기 버그 수정

--- app/src/main/java/com/example/planat/FindActivity.java
+++ app/src/main/java/com/example/planat/FindActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -51,25 +52,34 @@ public class FindActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onClick (View view){
             if (view == buttonFind) {
-                progressDialog.setMessage("처리중입니다. 잠시 기다려 주세요...");
-                progressDialog.show();
                 //비밀번호 재설정 이메일 보내기
-                String emailAddress = editTextUserEmail.getText().toString().trim();
-                firebaseAuth.sendPasswordResetEmail(emailAddress)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(FindActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
-                                    finish();
-                                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                                } else {
-                                    Toast.makeText(FindActivity.this, "메일 보내기 실패!", Toast.LENGTH_LONG).show();
+                if(editTextUserEmail.getText().toString().equals("")){
+                    Toast.makeText(this, "이메일을 입력해주세요!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(editTextUserEmail.getText().toString().indexOf("@")>=0){
+                    progressDialog.setMessage("처리중입니다. 잠시 기다려 주세요...");
+                    progressDialog.show();
+                    String emailAddress = editTextUserEmail.getText().toString().trim();
+                    firebaseAuth.sendPasswordResetEmail(emailAddress)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(FindActivity.this, "이메일을 보냈습니다.", Toast.LENGTH_LONG).show();
+                                        finish();
+                                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                    } else {
+                                        Toast.makeText(FindActivity.this, "메일 보내기 실패!", Toast.LENGTH_LONG).show();
+                                    }
+                                    progressDialog.dismiss();
                                 }
-                                progressDialog.dismiss();
-                            }
-                        });
-
+                            });
+                }
+                else{
+                    Toast.makeText(this, "이메일 형식으로 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
             if (view == back) {
                 finish();
